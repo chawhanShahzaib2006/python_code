b8137075ff9c5f8e85f46bb279b4bd5bfd7c4eb6
chawhanShahzaib2006 <chawhanshahzaib@gmail.com>
2021-07-12 20:52:24 -0700

    My movie recommendation engine

---
+++ Movie recommendation engine.py
@@ -0,0 +1,16 @@
+genreWanted = input("Please enter the genre of movies you would like to get recommended.")
+languageWanted = input("Please enter the language you want only available are Hindi and English.")
+
+if genreWanted == "comedy" and languageWanted == "english":
+    print("The movies I would recommend are: Jumanji The next level , Boss baby and , Kung fu Panda  ")
+elif genreWanted == "comedy" and languageWanted == "hindi":
+    print("The movies I would recommend are: Fukrey , 3 idiots , and Roohi ")   
+elif genreWanted == "action" and languageWanted == "english":
+    print("The movies I would recommend are: Avengers End game , Ant-man , and The Hitman's bodyguard ")   
+elif genreWanted == "action" and languageWanted == "hindi":
+    print("The movies I would recommend are: Ek tha tiger , Don , and    ")   
+elif genreWanted == "romantic" and languageWanted == "english":
+    print("The movies I would recommend are: Fukrey , 3 idiots , and Roohi ")   
+elif genreWanted == "romantic" and languageWanted == "hindi":
+    print("The movies I would recommend are: Fukrey , 3 idiots , and Roohi ")   
+    
